91bb36fbafd129bac8b714b313715864a5d187cb
Oisin Larkin <liballoisinl@gmail.com>
2024-07-29 19:09:36 +0100

    initial commit

---
+++ controllers/station-controller.js
@@ -0,0 +1,28 @@
+import { stationStore } from "../models/station-store.js";
+import { reportStore } from "../models/report-store.js";
+
+
+export const stationController = {
+  async index(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const viewData = {
+      title: "Station",
+      station: station,
+    };
+    response.render("station-view", viewData);
+  },
+
+  async addReport(request, response) {
+    const station = await stationStore.getStationById(request.params.id);
+    const newReport = {
+      code: Number(request.body.code),
+      temp: Number(request.body.temp),
+      wind_speed: Number(request.body.wind_speed),
+      pressure: Number(request.body.pressure),
+    };
+    console.log(`adding report ${newReport.code}`);
+    await reportStore.addReport(station._id, newReport);
+    response.redirect("/station/" + station._id);
+  },
+
+};
